e1711a0f0330c6a2e4572215bbb7ed60cc064302
Amina Eltahir <68793775+Amna-A@users.noreply.github.com>
2020-09-09 23:43:25 -0600

    module 3 lab 3

---
+++ mod3lab3.py
@@ -0,0 +1,12 @@
+year = int(input("Enter a year: "))
+if year<1582:
+    print("Not within the Gregorian calendar period")
+
+if year%4!=0:
+    print(year,"is a common year")
+elif year%100!=100:
+    print(year,"is a leap year")
+elif year%400!=0:
+    print(year,"is a common year")
+else:
+    print(year,"is a leap year")
